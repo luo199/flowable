e2da2850a65eb47d5524324fa8407d5e27600c37
Luo <809615644@qq.com>
2020-09-01 16:36:01 +0800

    拉取项目

---
+++ flow-web/src/main/java/com/huasisoft/flow/business/web/CommPageController.java
@@ -0,0 +1,18 @@
+package com.huasisoft.flow.business.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+/**
+ * @Auther: flq
+ * @Description:
+ * @Date 2020/9/1
+ */
+@RestController
+@RequestMapping("/api/comm/page")
+@Api("通用页面视图接口")
+public class CommPageController {
+	
+	
+}
